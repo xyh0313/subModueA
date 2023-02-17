4e00e994fdad3d802a0bdcb4a03779126d6409fa
xinyanhui <user@userdeMacBook-Pro.local>
2023-02-17 10:57:27 +0800

    submodules update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*
  * @Description: 
  * @Date: 2023-02-16 15:59:43
- * @LastEditTime: 2023-02-16 16:00:35
+ * @LastEditTime: 2023-02-16 16:20:02
  * @Author: xinyanhui@haier.com
  */
 export function say() {
-    console.info('this is submodule A')
+    console.info('this is submodule A update')
 }
